9acc0cd83b07033828004b2dfbbb619f1a7694ca
jiajun <jiajunhangg@gmail.com>
2021-12-27 03:34:02 +0800

    api fetch done for quiz builder component

--- src/admin/Template.js
+++ src/admin/Template.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,58 +10,154 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { styled } from '@mui/material/styles';
 
-function createData(name, collection, length, estimator, surveyEnabled) {
-  return { name, collection, length, estimator, surveyEnabled };
-}
-
-const rows = [
-  createData('IS2103 - Group 1', "collection_1", 15, "STD", "Yes"),
-  createData('IS2103 - Group 2', "collection_1", 15, "MLE", "Yes"),
-  createData('IS2103 - Group 3', "collection_1", 15, "EAP", "Yes"),
-];
+const Input = styled('input')({
+  display: 'none',
+});
 
 const Template = () => {
 
+  const [quizzes, setQuizzes] = useState(null);
+  const [pools, setPools] = useState(null);
+
+  const [open, setOpen] = useState(false);
+  const [newQuizName, setNewQuizName] = useState("");
+  const [newQuizLength, setNewQuizLength] = useState();
+  const [newQuizEstimator, setNewQuizEstimator] = useState("");
+  const [newQuizSurvey, setNewQuizSurvey] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleNameChange = (e) => {
+    setNewQuizName(e.target.value);
+  }
+
+  const handleLengthChange = (e) => {
+    setNewQuizLength(e.target.value);
+  }
+
+  const handleEstimatorChange = (e) => {
+    setNewQuizEstimator(e.target.value);
+  }
+
+  const handleSurveyChange = (e) => {
+    setNewQuizSurvey(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    const body = {
+    }
+
+    console.log("Call api to create new quiz");
+  }
+
+  const quizzes_api = `http://localhost:5000/quizzes`;
+  const pools_api = `http://localhost:5000/pools`;
+
+  useEffect(() => {
+    console.log("useeffect")
+
+    axios.get(quizzes_api)
+      .then(res => setQuizzes(res.data))
+
+    axios.get(pools_api)
+      .then(res => setPools(res.data))
+  }, [])
+
   return (
-    <Box
-      sx={{
-        marginTop: 10,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}>
-      <Typography>TEMPLATE PAGE</Typography>
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Quiz Name</TableCell>
-            <TableCell align="right">Collection Name</TableCell>
-            <TableCell align="right">Total Length</TableCell>
-            <TableCell align="right">Estimator</TableCell>
-            <TableCell align="right">Survey Enabled</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.collection}</TableCell>
-              <TableCell align="right">{row.length}</TableCell>
-              <TableCell align="right">{row.estimator}</TableCell>
-              <TableCell align="right">{row.surveyEnabled}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </Box>
+    <>
+      {pools &&
+        <>
+          <Box
+            sx={{
+              marginTop: 10,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            <Grid container alignItems="stretch" direction="row" spacing={1}>
+              <Grid item xs={12}>
+                <Typography variant="h4">Overview of Quizzes</Typography>
+              </Grid>
+              <Grid item xs={12} justifyContent="flex-end">
+                <Button onClick={handleOpen} variant="contained" sx={{
+                  bgcolor: 'button.primary',
+                  '&:hover': {
+                    bgcolor: 'button.secondary'
+                  }
+                }}>New Quiz</Button>
+              </Grid>
+            </Grid>
+            <br></br>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 350 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Quiz Name</TableCell>
+                    <TableCell align="center">Collection</TableCell>
+                    <TableCell align="center">Quiz Length</TableCell>
+                    <TableCell align="center">Estimator</TableCell>
+                    <TableCell align="center">Survey</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {quizzes.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="center">{row.collection}</TableCell>
+                      <TableCell align="center">{row.length}</TableCell>
+                      <TableCell align="center">{row.estimator}</TableCell>
+                      <TableCell align="center">{String(row.survey)}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Create New Pool</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  To subscribe to this website, please enter your email address here. We
+                  will send updates occasionally.
+                </DialogContentText>
+                <TextField
+                  margin="dense"
+                  id="Quiz Name"
+                  label="Quiz Name"
+                  fullWidth
+                  variant="standard"
+                  onChange={handleNameChange}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button variant="contained" onClick={handleClose} sx={{
+                  bgcolor: 'button.primary',
+                  '&:hover': {
+                    bgcolor: 'button.secondary'
+                  }
+                }}>Upload</Button>
+              </DialogActions>
+            </Dialog>
+
+          </Box>
+        </>
+      }
+    </>
   );
 }
 
