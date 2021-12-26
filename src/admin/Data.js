e4cc7ee4b571ce3f949ff5eedabfec69f64e3a61
jiajun <jiajunhangg@gmail.com>
2021-12-27 03:20:29 +0800

    api fetch done for qn bank component

--- src/admin/Data.js
+++ src/admin/Data.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -18,8 +19,8 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { styled } from '@mui/material/styles';
 
-function createData(name, poolName, size) {
-  return { name, poolName, size };
+function createData(name, collection_name, size) {
+  return { name, collection_name, size };
 }
 
 const rows = [
@@ -32,10 +33,12 @@ const Input = styled('input')({
 
 const Data = () => {
 
+  const [pools, setPools] = useState(null);
+
   const [open, setOpen] = useState(false);
   const [newDataName, setNewDataName] = useState("");
   const [newCollectionName, setNewCollectionName] = useState("");
-  const [data, setData] = useState([]);
+  const [newData, setNewData] = useState([]);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -56,107 +59,120 @@ const Data = () => {
     const body = {
       name: newDataName,
       collectionName: newCollectionName,
-      data: data
+      data: newData
     }
 
     console.log("Call api to create question pool");
   }
 
+  const api = `http://localhost:5000/pools`;
+
+  useEffect(() => {
+    console.log("useeffect")
+    const res = axios.get(api)
+      .then(res => setPools(res.data))
+  }, [])
+
   return (
-    <Box
-      sx={{
-        marginTop: 10,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}>
-      <Grid container alignItems="stretch" direction="row" spacing={1}>
-        <Grid item xs={12}>
-          <Typography variant="h4">Overview of Collection</Typography>
-        </Grid>
-        <Grid item xs={12} justifyContent="flex-end">
-          <Button onClick={handleOpen} variant="contained" sx={{
-            bgcolor: 'button.primary',
-            '&:hover': {
-              bgcolor: 'button.secondary'
-            }
-          }}>New Collection</Button>
-        </Grid>
-      </Grid>
-      <br></br>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 350 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Pool Name</TableCell>
-              <TableCell align="center">Collection Name</TableCell>
-              <TableCell align="center">Pool Size</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="center">{row.poolName}</TableCell>
-                <TableCell align="center">{row.size}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Create New Pool</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            margin="dense"
-            id="Question Pool Name"
-            label="Question Pool Name"
-            fullWidth
-            variant="standard"
-            onChange={handleNameChange}
-          />
-          <TextField
-            margin="dense"
-            id="Collection Name"
-            label="Collection Name"
-            fullWidth
-            variant="standard"
-            onChange={handleCollectionNameChange}
-          />
-          <label htmlFor="contained-button-file">
-            <Input accept="application/JSON" id="contained-button-file" onChange={handleCapture} multiple type="file" />
-            <Button variant="contained" component="span" sx={{
-              bgcolor: 'button.primary',
-              '&:hover': {
-                bgcolor: 'button.secondary'
-              }
+    <>
+      {pools &&
+        <>
+          <Box
+            sx={{
+              marginTop: 10,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
             }}>
-              Upload
-            </Button>
-          </label>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="contained" onClick={handleClose} sx={{
-            bgcolor: 'button.primary',
-            '&:hover': {
-              bgcolor: 'button.secondary'
-            }
-          }}>Subscribe</Button>
-        </DialogActions>
-      </Dialog>
+            <Grid container alignItems="stretch" direction="row" spacing={1}>
+              <Grid item xs={12}>
+                <Typography variant="h4">Overview of Collection</Typography>
+              </Grid>
+              <Grid item xs={12} justifyContent="flex-end">
+                <Button onClick={handleOpen} variant="contained" sx={{
+                  bgcolor: 'button.primary',
+                  '&:hover': {
+                    bgcolor: 'button.secondary'
+                  }
+                }}>New Collection</Button>
+              </Grid>
+            </Grid>
+            <br></br>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 350 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Pool Name</TableCell>
+                    <TableCell align="center">Collection Name</TableCell>
+                    <TableCell align="center">Pool Size</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {pools.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="center">{row.collection_name}</TableCell>
+                      <TableCell align="center">{row.size}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
 
-    </Box>
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Create New Pool</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  To subscribe to this website, please enter your email address here. We
+                  will send updates occasionally.
+                </DialogContentText>
+                <TextField
+                  margin="dense"
+                  id="Question Pool Name"
+                  label="Question Pool Name"
+                  fullWidth
+                  variant="standard"
+                  onChange={handleNameChange}
+                />
+                <TextField
+                  margin="dense"
+                  id="Collection Name"
+                  label="Collection Name"
+                  fullWidth
+                  variant="standard"
+                  onChange={handleCollectionNameChange}
+                />
+                <label htmlFor="contained-button-file">
+                  <Input accept="application/JSON" id="contained-button-file" onChange={handleCapture} multiple type="file" />
+                  <Button variant="contained" component="span" sx={{
+                    bgcolor: 'button.primary',
+                    '&:hover': {
+                      bgcolor: 'button.secondary'
+                    }
+                  }}>
+                    Select JSON
+                  </Button>
+                </label>
+              </DialogContent>
+              <DialogActions>
+                <Button variant="contained" onClick={handleClose} sx={{
+                  bgcolor: 'button.primary',
+                  '&:hover': {
+                    bgcolor: 'button.secondary'
+                  }
+                }}>Upload</Button>
+              </DialogActions>
+            </Dialog>
 
+          </Box>
+        </>
+      }
+    </>
   );
 }
 
