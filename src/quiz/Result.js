2fe0eed90f827ea4e79798f7f308574b8a3b6b46
jiajun <jiajunhangg@gmail.com>
2022-01-02 02:11:58 +0800

    added basic analytics onto result page

--- src/quiz/Result.js
+++ src/quiz/Result.js
@@ -4,6 +4,9 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Loader from '../components/Loader';
 import axios from 'axios';
+import Divider from '@mui/material/Divider';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 const Result = ({ data, quizData }) => {
 
@@ -17,8 +20,6 @@ const Result = ({ data, quizData }) => {
       data: data,
       quizData: quizData,
     };
-
-    console.log(JSON.stringify(body));
 
     const response = await axios.post(api, body);
     return response;
@@ -58,7 +59,7 @@ const Result = ({ data, quizData }) => {
         }}>
           <Grid container spacing={2}>
             <Grid item md={12}>
-              <Typography variant="h4">Summary of Quiz</Typography>
+              <Typography variant="h4">Performance Summary</Typography>
             </Grid>
             {Object.entries(result.summary).map(([k, v], i) =>
               <Grid container item key={k}>
@@ -70,6 +71,46 @@ const Result = ({ data, quizData }) => {
                 </Grid>
               </Grid>
             )}
+          </Grid>
+          <br />
+          <Divider />
+          <br />
+          <Grid container spacing={1}>
+            <Grid item md={12}>
+              <Typography variant="h4">Performance Review</Typography>
+            </Grid>
+            {result && result.detail &&
+              Object.entries(result.detail.questions).map((qn, i) =>
+                <>
+                  <Grid item md={12}>
+                    <Typography variant='h5'>Question {i + 1}: </Typography>
+                  </Grid>
+                  <Grid item md={12}>
+                    <Typography variant='h5'>{qn[1].question} </Typography>
+                  </Grid>
+                  <Grid item md={12}>
+                    <ToggleButtonGroup disabled orientation="vertical" fullWidth={true}>
+                      {console.log(qn[1].options)}
+                      {qn[1].options && qn[1].options.map((e, idx) =>
+                        <ToggleButton key={idx + 1} value={idx+1} selected={result.detail.responses[i] === idx + 1} label={idx + 1 + ". " + e} sx={{
+                          justifyContent: "flex-start"
+                        }}>
+                          {console.log('idx:' + idx)}
+                          {console.log(result.detail.responses[idx])}
+                          {idx + 1 + ". " + e}
+                        </ToggleButton>
+                      )}
+                    </ToggleButtonGroup>
+                  </Grid>
+                  <Grid item md={12}>
+                    <Typography>Correct Answer: {qn[1].correct}</Typography>
+                    {qn[1].explanation && <Typography >{qn[1].explanation}</Typography>}
+                    <Typography >{100 * qn[1]['total_correct'] / qn[1]['total_attempts']}% of students have answered correctly.</Typography>
+                  <p></p>
+                  <Divider />
+                  </Grid>
+                </>
+              )}
           </Grid>
         </Box>
       }
