9b851ea94f4b4c86401701e1ed495e63b586faac
jiajun <jiajunhangg@gmail.com>
2022-03-27 11:42:23 +0800

    updated result page

--- src/quiz/CalibResult.js
+++ src/quiz/CalibResult.js
@@ -7,8 +7,15 @@ const CalibResult = () => {
 
   return (
     <>
-      <h3>Result ID: ${id}</h3>
-      <h3>Completed submission. Pls update the Result ID to Prof. Tan.</h3>
+      <h2>Result ID: {id}</h2>
+      <h3>Completed submission, please fill in the details in the <a href="https://forms.office.com/r/5YZVSvyAFk">form</a> provided.</h3>
+        You will need to provide the following:
+        <ul>Name: YOUR NAME</ul>
+        <ul>Student Number: NUS Matric Number</ul>
+        <ul>Email: NUS email</ul>
+        <ul>Mobile: Mobile Number</ul>
+        <ul>Result ID: {id}</ul>
+      <h3>**NOTE**: For #5 in the form, fill in Result ID (i.e. {id}, NOT session ID that was created from the start!) </h3>
     </>
   );
 }
